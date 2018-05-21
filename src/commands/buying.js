0ae9154036c1695b69b913e18c17503903af894e
Jonathan Martinez <jonathan.martinez1217@gmail.com>
2018-05-20 20:42:09 -0500

    Trying to call slack groupsinfo api method

--- src/commands/buying.js
+++ src/commands/buying.js
@@ -1,17 +1,123 @@
 
+'use strict'
+
+const _ = require('lodash')
+const config = require('../config')
+
+const msgDefaults = {
+  response_type: 'in_channel',
+  username: 'Starbot',
+  icon_emoji: config('ICON_EMOJI')
+}
+
+var request = require('request');
+var url = 'https://slack.com/api/groups.info?token=' + 
+          process.env.APITOKEN + 
+          '&channel=' +
+          process.env.CHANNEL +
+          '&pretty=1';
+
+request.get({
+  url: url,
+  json: true,
+  headers: {'User-Agent': 'request'}
+}, (err, res, data) => {
+  if (err) {
+    console.log('Error:', err);
+  } else if (res.statusCode !== 200) {
+    console.log('Status:', res.statusCode);
+  } else {
+    // data is already parsed as JSON:
+    console.log(data.html_url);
+    let attachments = [
+      {
+        title: 'Buying This Week',
+        color: '#2FA44F',
+        text: data.html_url,
+        mrkdwn_in: ['text']
+      },
+      {
+        title: 'On Deck:',
+        color: '#E3E4E6',
+        text: 'Somebody',
+        mrkdwn_in: ['text']
+      },
+      {
+        title: 'In the hole:',
+        color: '#E3E4E6',
+        text: 'NoBody',
+        mrkdwn_in: ['text']
+      }
+    ]
+
+    const handler = (payload, res) => {
+      let msg = _.defaults({
+        channel: payload.channel_name,
+        attachments: attachments
+      }, msgDefaults)
+
+      res.set('content-type', 'application/json')
+      res.status(200).json(msg)
+      return
+    }
+
+    module.exports = { pattern: /buying/ig, handler: handler }
+  }
+})
+
+
+
+
+// 'use strict';
+// var request = require('request');
+
+// var url = 'https://api.github.com/users/rsp';
+
+// request.get({
+//     url: url,
+//     json: true,
+//     headers: {'User-Agent': 'request'}
+//   }, (err, res, data) => {
+//     if (err) {
+//       console.log('Error:', err);
+//     } else if (res.statusCode !== 200) {
+//       console.log('Status:', res.statusCode);
+//     } else {
+//       // data is already parsed as JSON:
+//       console.log(data.html_url);
+//     }
+// });
+
+
+
+
+// request.get({
+//     url: url,
+//     json: true,
+//     headers: {'User-Agent': 'request'}
+//   }, (err, res, data) => {
+//     if (err) {
+//       console.log('Error:', err);
+//     } else if (res.statusCode !== 200) {
+//       console.log('Status:', res.statusCode);
+//     } else {
+//       // data is already parsed as JSON:
+//       console.log(data.html_url);
+//     }
+// });
+
+// ===================
 // 'use strict'
 
 // const _ = require('lodash')
 // const config = require('../config')
 // const trending = require('github-trending')
 
-
 // const msgDefaults = {
 //   response_type: 'in_channel',
-//   username: 'Rounds',
+//   username: 'Starbot',
 //   icon_emoji: config('ICON_EMOJI')
 // }
-
 
 // const handler = (payload, res) => {
 //   trending('javascript', (err, repos) => {
@@ -38,48 +144,3 @@
 // }
 
 // module.exports = { pattern: /repos/ig, handler: handler }
-
-'use strict'
-
-const _ = require('lodash')
-const config = require('../config')
-
-const msgDefaults = {
-  response_type: 'in_channel',
-  username: 'Starbot',
-  icon_emoji: config('ICON_EMOJI')
-}
-
-let attachments = [
-  {
-    title: 'Buying This Week',
-    color: '#2FA44F',
-    text: 'Someone',
-    mrkdwn_in: ['text']
-  },
-  {
-    title: 'On Deck:',
-    color: '#E3E4E6',
-    text: 'Somebody',
-    mrkdwn_in: ['text']
-  },
-  {
-    title: 'In the hole:',
-    color: '#E3E4E6',
-    text: 'NoBody',
-    mrkdwn_in: ['text']
-  }
-]
-
-const handler = (payload, res) => {
-  let msg = _.defaults({
-    channel: payload.channel_name,
-    attachments: attachments
-  }, msgDefaults)
-
-  res.set('content-type', 'application/json')
-  res.status(200).json(msg)
-  return
-}
-
-module.exports = { pattern: /buying/ig, handler: handler }
