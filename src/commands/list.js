1efeb152ef29c00a2e3b59bd42a74e2194bf1dd3
Jonathan Martinez <jonathan.martinez1217@gmail.com>
2019-05-15 21:08:50 -0500

    log

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,70 +1,70 @@
 
-'use strict'
+// 'use strict'
 
-const _ = require('lodash')
-const config = require('../config')
-const https = require('https');
+// const _ = require('lodash')
+// const config = require('../config')
+// const https = require('https');
 
-const msgDefaults = {
-  response_type: 'in_channel',
-  username: 'Starbot',
-  icon_emoji: config('ICON_EMOJI')
-}
-var mandatory = require('mandatory');
-var VError = require('verror');
-var request = require('request');
+// const msgDefaults = {
+//   response_type: 'in_channel',
+//   username: 'Starbot',
+//   icon_emoji: config('ICON_EMOJI')
+// }
+// var mandatory = require('mandatory');
+// var VError = require('verror');
+// var request = require('request');
 
- var obj = {}; 
- obj[ 'bar' ] = true;       
- obj[ 'foo' ] = false; 
+//  var obj = {}; 
+//  obj[ 'bar' ] = true;       
+//  obj[ 'foo' ] = false; 
 
-console.log(obj)
-console.log(obj.bar)
-console.log(obj[0])
-var turn = 0
-// function checkDate() {
-//     var date = new Date();
-//     console.log(date.getDay());
-//     console.log(date.getHours());
-//     if(date.getDay() === 6 && date.getHours() === 17) {
-//         console.log("HELLO WORLD!");
+// console.log(obj)
+// console.log(obj.bar)
+// console.log(obj[0])
+// var turn = 0
+// // function checkDate() {
+// //     var date = new Date();
+// //     console.log(date.getDay());
+// //     console.log(date.getHours());
+// //     if(date.getDay() === 6 && date.getHours() === 17) {
+// //         console.log("HELLO WORLD!");
+// //     }
+// // }
+
+// // var dateLoop = setInterval(function() {
+// //     checkDate();
+// // },5000);
+
+
+// const handler = (payload, res) => {
+//   let attachments = [
+//     {
+//       text: 'obj',
+//     },
+//     {
+//       title: 'On Deck:',
+//       color: '#E3E4E6',
+//       text: turn + 1,
+//       mrkdwn_in: ['text']
+//     },
+//     {
+//       title: 'In the hole:',
+//       color: '#E3E4E6',
+//       text: turn + 2,
+//       mrkdwn_in: ['text']
 //     }
+//   ]
+  
+//   let msg = _.defaults({
+//     channel: payload.channel_name,
+//     attachments: attachments
+//   }, msgDefaults)
+  
+  
+
+//   res.set('content-type', 'application/json')
+//   res.status(200).json(msg)
+//   return
 // }
 
-// var dateLoop = setInterval(function() {
-//     checkDate();
-// },5000);
-
-
-const handler = (payload, res) => {
-  let attachments = [
-    {
-      text: obj,
-    },
-    {
-      title: 'On Deck:',
-      color: '#E3E4E6',
-      text: turn + 1,
-      mrkdwn_in: ['text']
-    },
-    {
-      title: 'In the hole:',
-      color: '#E3E4E6',
-      text: turn + 2,
-      mrkdwn_in: ['text']
-    }
-  ]
-  
-  let msg = _.defaults({
-    channel: payload.channel_name,
-    attachments: attachments
-  }, msgDefaults)
-  
-  
-
-  res.set('content-type', 'application/json')
-  res.status(200).json(msg)
-  return
-}
-
-module.exports = { pattern: /test/ig, handler: handler }
+// module.exports = { pattern: /test/ig, handler: handler }
