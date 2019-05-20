709e04dd18e8257179c5dadef53acd5cca097328
Jonathan Martinez <jonathan.martinez1217@gmail.com>
2019-05-20 00:00:37 -0500

    copied the git file

--- src/commands/buying.js
+++ src/commands/buying.js
@@ -1,70 +1,231 @@
+'use strict'
 
+const _ = require('lodash')
+const config = require('../config')
+const https = require('https');
+
+const msgDefaults = {
+  response_type: 'in_channel',
+  username: 'Starbot',
+  icon_emoji: config('ICON_EMOJI')
+}
+var mandatory = require('mandatory');
+var VError = require('verror');
+var request = require('request');
+var url = 'https://api.github.com/users/rsp';
+
+var bar = ['Lauren'
+,'Tom K '
+,'Ryan S'
+,'Brad A'
+,'Mike W.'
+,'Mary S.' 
+,'Vlad '
+,'Avi'
+,'Bill H.'
+,'Emily'
+,'Chris D.'
+,'Jon M.'
+,'Chris B.'
+];
+
+ var obj = {}; 
+ obj[ 'bar' ] = "value3";       
+ obj[ 'foo' ] = "value3"; 
+
+var turn = 0
+// function checkDate() {
+//     var date = new Date();
+//     console.log(date.getDay());
+//     console.log(date.getHours());
+//     if(date.getDay() === 6 && date.getHours() === 17) {
+//         console.log("HELLO WORLD!");
+//     }
+// }
+
+// var dateLoop = setInterval(function() {
+//     checkDate();
+// },5000);
+
+
+const handler = (payload, res) => {
+  let attachments = [
+    {
+      title: 'Buying This Week',
+      color: '#2FA44F',
+      text: bar[turn],
+      mrkdwn_in: ['text']
+    },
+    {
+      title: 'On Deck:',
+      color: '#E3E4E6',
+      text: bar[turn + 1],
+      mrkdwn_in: ['text']
+    },
+    {
+      title: 'In the hole:',
+      color: '#E3E4E6',
+      text: bar[turn + 2],
+      mrkdwn_in: ['text']
+    }
+  ]
+  
+  let msg = _.defaults({
+    channel: payload.channel_name,
+    attachments: attachments
+  }, msgDefaults)
+  
+  
+
+  res.set('content-type', 'application/json')
+  res.status(200).json(msg)
+  return
+}
+
+module.exports = { pattern: /buying/ig, handler: handler }
+
+
+// 'use strict';
+// var request = require('request');
+
+// var url = 'https://api.github.com/users/rsp';
+
+// request.get({
+//     url: url,
+//     json: true,
+//     headers: {'User-Agent': 'request'}
+//   }, (err, res, data) => {
+//     if (err) {
+//       console.log('Error:', err);
+//     } else if (res.statusCode !== 200) {
+//       console.log('Status:', res.statusCode);
+//     } else {
+//       // data is already parsed as JSON:
+//       console.log(data.html_url);
+//     }
+// });
+
+
+
+
+// request.get({
+//     url: url,
+//     json: true,
+//     headers: {'User-Agent': 'request'}
+//   }, (err, res, data) => {
+//     if (err) {
+//       console.log('Error:', err);
+//     } else if (res.statusCode !== 200) {
+//       console.log('Status:', res.statusCode);
+//     } else {
+//       // data is already parsed as JSON:
+//       console.log(data.html_url);
+//     }
+// });
+
+// ===================
 // 'use strict'
 
 // const _ = require('lodash')
 // const config = require('../config')
-// const https = require('https');
+// const trending = require('github-trending')
 
 // const msgDefaults = {
 //   response_type: 'in_channel',
 //   username: 'Starbot',
 //   icon_emoji: config('ICON_EMOJI')
 // }
-// var mandatory = require('mandatory');
-// var VError = require('verror');
-// var request = require('request');
-
-//  // var obj = {}; 
-//  // obj[ 'bar' ] = true;       
-//  // obj[ 'foo' ] = false; 
-//  // obj[ 'dog' ] = false; 
-
-// // console.log(obj)
-// // console.log(obj.bar)
-
-// var turn = 0
-// // function checkDate() {
-// //     var date = new Date();
-// //     console.log(date.getDay());
-// //     console.log(date.getHours());
-// //     if(date.getDay() === 6 && date.getHours() === 17) {
-// //         console.log("HELLO WORLD!");
-// //     }
-// // }
-
-// // var dateLoop = setInterval(function() {
-// //     checkDate();
-// // },5000);
-
 
 // const handler = (payload, res) => {
-//   let attachments = [
-//     {
-//       title: 'Buying This Week',
-//       color: '#2FA44F',
-//       text: 'turn',
-//       mrkdwn_in: ['text']
-//     },
-//     {
-//       title: 'On Deck:',
-//       color: '#E3E4E6',
-//       text: 'turn' ,
-//       mrkdwn_in: ['text']
-//     },
-//     {
-//       title: 'In the hole:',
-//       color: '#E3E4E6',
-//       text: 'turn',
-//       mrkdwn_in: ['text']
+//   trending('javascript', (err, repos) => {
+//     if (err) throw err
+
+//     var attachments = repos.slice(0, 5).map((repo) => {
+//       return {
+//         title: `${repo.owner}/${repo.title} `,
+//         title_link: repo.url,
+//         text: `_${repo.description}_\n${repo.language} • ${repo.star}>`,
+//         mrkdwn_in: ['text', 'pretext']
+//       }
+//     })
+
+//     let msg = _.defaults({
+//       channel: payload.channel_name,
+//       attachments: attachments
+//     }, msgDefaults)
+
+//     res.set('content-type', 'application/json')
+//     res.status(200).json(msg)
+//     return
+//   })
+// }
+
+// module.exports = { pattern: /repos/ig, handler: handler }
+
+
+
+
+// 'use strict'
+
+// const _ = require('lodash')
+// const config = require('../config')
+
+// const msgDefaults = {
+//   response_type: 'in_channel',
+//   username: 'Rounds',
+//   icon_emoji: config('ICON_EMOJI')
+// }
+
+// var request = require('request');
+// var url = 'https://api.github.com/users/rsp';
+
+// function getData() {
+
+//   request.get({
+
+//     url: url,
+//     json: true,
+//     headers: {'User-Agent': 'request'}
+//   }, (err, res, data) => {
+//     if (err) {
+//       return 'Error:' + err;
+//     } else if (res.statusCode !== 200) {
+//       return 'Status:' + res.statusCode;
+//     } else {
+//       // data is already parsed as JSON:
+//       return "YES";
 //     }
-//   ]
-  
+//   });
+// }
+
+
+// var theData = getData()
+// var attachments = [
+//   {
+//     title: 'Buying This Week',
+//     color: '#2FA44F',
+//     text: theData,
+//     mrkdwn_in: ['text']
+//   },
+//   {
+//     title: 'On Deck:',
+//     color: '#E3E4E6',
+//     text: 'Somebody',
+//     mrkdwn_in: ['text']
+//   },
+//   {
+//     title: 'In the hole:',
+//     color: '#E3E4E6',
+//     text: 'NoBody',
+//     mrkdwn_in: ['text']
+//   }
+// ]
+
+// const handler = (payload, res) => {
 //   let msg = _.defaults({
 //     channel: payload.channel_name,
 //     attachments: attachments
 //   }, msgDefaults)
-  
-  
 
 //   res.set('content-type', 'application/json')
 //   res.status(200).json(msg)
